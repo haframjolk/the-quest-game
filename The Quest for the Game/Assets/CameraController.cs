197d63ab66578eb0c0478aa5725437a29d6f6b85
Reyn Alpha <reynalpha@outlook.com>
2020-02-18 16:03:22 +0000

    Create camera system

---
+++ The Quest for the Game/Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        // Move camera with player, always keeping them centered
+        transform.position = player.transform.position + offset;
+    }
+}
