31787db516682a5a13665e72bc480f1d4c647674
Reyn Alpha <reynalpha@outlook.com>
2020-03-19 12:16:39 +0000

    Initial work on Raudalaekur opening cutscene

---
+++ The Quest for the Game/Assets/Scripts/AnimationSignalHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSignalHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    // Stilla hvert leikmaður snýr (fyrir timeline events)
+    public void SetAnimatorDirection(int direction)
+    {
+        animator.SetInteger("Direction", direction);
+    }
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+}
