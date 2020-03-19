17f125bea413a97c2adf217683ead2ee43662266
Reyn Alpha <reynalpha@outlook.com>
2020-03-19 12:03:07 +0000

    Make transition load next scene

--- The Quest for the Game/Assets/Scripts/EventHandler.cs
+++ The Quest for the Game/Assets/Scripts/EventHandler.cs
@@ -5,12 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class EventHandler : MonoBehaviour
 {
+    // Hlaða inn aðalvalmynd
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Hlaða inn senum
+    public void LoadSvennzHouse()
+    {
+        SceneManager.LoadScene("SvennzHouse");
+    }
+
+    public void LoadRaudalaekur()
+    {
+        SceneManager.LoadScene("Raudalaekur");
+    }
+
+    public void LoadNewZealand()
+    {
+        SceneManager.LoadScene("NewZealand");
+    }
+
     void Update()
     {
         // Ef leikmaður ýtir á cancel takka (sjálfgefið esc), fara í aðalvalmynd
         if (Input.GetButton("Cancel"))
         {
-            SceneManager.LoadScene("MainMenu");
+            LoadMainMenu();
         }
     }
 }
