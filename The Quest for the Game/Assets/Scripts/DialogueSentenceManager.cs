9342eaf10bb3e623741d497ee98c56547c0c7e2c
Reyn Alpha <reynalpha@outlook.com>
2020-03-26 11:42:27 +0000

    Implement multiple sets of dialogue, add SvennzHouse text

---
+++ The Quest for the Game/Assets/Scripts/DialogueSentenceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSentenceManager : MonoBehaviour
+{
+    public Dialogue[] dialogueArray;
+    private DialogueManager dialogueManager;
+    private int currentId = 0;
+
+    void Start()
+    {
+        dialogueManager = GetComponent<DialogueManager>();
+    }
+
+    public void TriggerDialogue(int id)
+    {
+        dialogueManager.StartDialogue(dialogueArray[id]);
+    }
+
+    // Sýna næsta dialogue-box (hækka id um 1)
+    public void TriggerNextDialogue()
+    {
+        TriggerDialogue(currentId++);
+    }
+}
