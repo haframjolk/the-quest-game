5ef27a71e993d910cd674705a610cb3b345bc7ca
Reyn Alpha <reynalpha@outlook.com>
2020-03-26 11:12:26 +0000

    Improve comments

--- The Quest for the Game/Assets/Scripts/TimelineController.cs
+++ The Quest for the Game/Assets/Scripts/TimelineController.cs
@@ -14,11 +14,13 @@ public class TimelineController : MonoBehaviour
 
     public void PauseTimeline()
     {
+        // Minnkar hraðann á tímalínunni í 0 svo hún hætti að hreyfast en hætti þó ekki að vera „evaluated“
         director.playableGraph.GetRootPlayable(0).SetSpeed(0);
     }
 
     public void PlayTimeline()
     {
+        // Stillir hraðann aftur á 1 svo tímalínan haldi venjulega áfram
         director.playableGraph.GetRootPlayable(0).SetSpeed(1);
     }
 }
