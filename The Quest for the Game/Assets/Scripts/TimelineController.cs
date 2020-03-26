2ffa99767cfad48a95f936cdbf99e45ac5976d98
Reyn Alpha <reynalpha@outlook.com>
2020-03-26 11:10:28 +0000

    Tie dialogue box with timeline (pause timeline while showing text)

---
+++ The Quest for the Game/Assets/Scripts/TimelineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    private PlayableDirector director;
+
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();    
+    }
+
+    public void PauseTimeline()
+    {
+        director.playableGraph.GetRootPlayable(0).SetSpeed(0);
+    }
+
+    public void PlayTimeline()
+    {
+        director.playableGraph.GetRootPlayable(0).SetSpeed(1);
+    }
+}
