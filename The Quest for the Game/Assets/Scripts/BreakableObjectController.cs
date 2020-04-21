65982bae02e60674f638a64b75e06800db8df292
Reyn Alpha <reynalpha@outlook.com>
2020-04-21 16:50:01 +0000

    Add error sound to breakable objects

--- The Quest for the Game/Assets/Scripts/BreakableObjectController.cs
+++ The Quest for the Game/Assets/Scripts/BreakableObjectController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class BreakableObjectController : InteractableController
 {
     public bool isBreakable = true;
+    public AudioClip errorClip;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void SetBreakableStatus(bool value)
     {
@@ -17,11 +24,16 @@ public class BreakableObjectController : InteractableController
         {
             Destroy(this.gameObject);
         }
+        // Spila hljóð ef ekki er hægt að brjóta hlut
+        else if (errorClip && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(errorClip);
+        }
     }
 
     public override void Interact()
     {
-        Debug.Log("Player interacted with breakable object.");
+        // Debug.Log("Player interacted with breakable object.");
         BreakObject();
     }
 }
