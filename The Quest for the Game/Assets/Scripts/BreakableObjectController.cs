9fd19edb31dcd8a202848e5e6572f55e6c6d5ad8
Reyn Alpha <reynalpha@outlook.com>
2020-04-21 16:26:03 +0000

    Add interactable, breakable car

---
+++ The Quest for the Game/Assets/Scripts/BreakableObjectController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObjectController : InteractableController
+{
+    public bool isBreakable = true;
+
+    public void SetBreakableStatus(bool value)
+    {
+        isBreakable = value;
+    }
+
+    public void BreakObject()
+    {
+        if (isBreakable)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Player interacted with breakable object.");
+        BreakObject();
+    }
+}
