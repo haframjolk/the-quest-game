b338dca36a423f63e7fbf95cacb4e1e856b14ff5
Reyn Alpha <reynalpha@outlook.com>
2020-04-21 17:16:47 +0000

    Add stone collectible

---
+++ The Quest for the Game/Assets/Scripts/CollectibleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleController : InteractableController
+{
+    public AudioClip collectClip;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    IEnumerator WaitThenDestroy()
+    {
+        float timeToWait = 0f;
+        if (collectClip)
+        {
+            timeToWait = collectClip.length;
+        }
+        yield return new WaitForSeconds(timeToWait);
+        Destroy(gameObject);
+    }
+
+    public virtual void Collect()
+    {
+        float timeToWait = 0f;
+        // Spila hljóð þegar notandi nær í hlutinn
+        if (collectClip && !audioSource.isPlaying)
+        {
+            timeToWait = collectClip.length;
+            audioSource.PlayOneShot(collectClip);
+        }
+        // Slökkva á collider og renderer á meðan hljóðið spilast, áður en hlutnum er eytt, svo hann hverfi strax
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
+        // Eyða honum þegar hljóðið er búið að spilast
+        StartCoroutine("WaitThenDestroy");
+    }
+    public override void Interact()
+    {
+        Collect();
+    }
+}
