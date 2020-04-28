4e350c8b638349125e28559686b300a0fbfcb9cb
Reyn Alpha <reynalpha@outlook.com>
2020-04-28 17:50:55 +0000

    Add Rasmusen and Kiwi cutscenes, allow player to pass through border

---
+++ The Quest for the Game/Assets/Scripts/InteractableNPCController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class InteractableNPCController : InteractableController
+{
+    public PlayableDirector timeline;
+
+    public override void Interact()
+    {
+        timeline.Play();
+    }
+}
