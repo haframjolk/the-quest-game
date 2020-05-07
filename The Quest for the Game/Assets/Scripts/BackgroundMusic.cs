d45fcbcc76c8f00ea3d7c11da7a1deaa5a4f0db0
Reyn Alpha <reynalpha@outlook.com>
2020-05-07 11:52:22 +0000

    Add background music to New Zealand and YonasCastle, stop it before starting Yonas fight

---
+++ The Quest for the Game/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+
+    public void StopSmooth()
+    {
+        FadeToEnd(0.25f);
+    }
+
+    // Láta hljóð fjara út
+    public IEnumerator FadeOut(float duration)
+    {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / duration;
+            yield return null;
+        }
+        Stop();
+    }
+
+    public void FadeToEnd(float duration)
+    {
+        StartCoroutine(FadeOut(duration));
+    }
+}
